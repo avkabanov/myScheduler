c0548546de67312a54864dcdfb95d666160382ff
avkabanov <usrobotics1>
2018-10-03 20:59:11 +0300

    fixed notifications!!

--- app/src/main/java/com/kabanov/scheduler/notification/NotificationGenerator.java
+++ app/src/main/java/com/kabanov/scheduler/notification/NotificationGenerator.java
@@ -1,53 +1,53 @@
 package com.kabanov.scheduler.notification;
 
+import com.kabanov.scheduler.MainActivity;
+import com.kabanov.scheduler.R;
+
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
-
-import com.kabanov.scheduler.MainActivity;
-import com.kabanov.scheduler.R;
 
 public class NotificationGenerator {
 
     @SuppressWarnings("static-access")
     public static void generateNotification(Context context, int numberOfOverdueActions) {
+        displayNotification(context, "Brother! Some items are overdue!",
+                numberOfOverdueActions + " items are overdue! Take a look");
+    }
+
+    private static void displayNotification(Context context, String title, String contentText) {
         Intent resultIntent = new Intent(context, MainActivity.class);
 
-        NotificationCompat.Builder nb = new NotificationCompat.Builder(context);
-        nb.setSmallIcon(R.drawable.ic_launcher_foreground);
-        nb.setContentTitle("Brother , Some items are overdue!");
-        nb.setContentText(numberOfOverdueActions + " items are overdue! Take a look");
-        nb.setTicker("Take a look");
-        nb.setAutoCancel(true);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        int notificationId = 1;
+        String channelId = "channel-01";
+        String channelName = "Channel Name";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
 
-        //get the bitmap to show in notification bar
-        Bitmap bitmap_image = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_foreground);
-        NotificationCompat.BigPictureStyle s = new NotificationCompat.BigPictureStyle().bigPicture(bitmap_image);
-        s.setSummaryText(numberOfOverdueActions + " items are overdue! Take a look");
-        nb.setStyle(s);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(
+                    channelId, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
 
-        TaskStackBuilder TSB = TaskStackBuilder.create(context);
-        TSB.addParentStack(MainActivity.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        TSB.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                TSB.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(contentText);
 
-        nb.setContentIntent(resultPendingIntent);
-        nb.setAutoCancel(true);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        mBuilder.setContentIntent(resultPendingIntent);
 
-        // mId allows you to update the notification later on.
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(11221, nb.build());
+        notificationManager.notify(notificationId, mBuilder.build());
     }
 }
