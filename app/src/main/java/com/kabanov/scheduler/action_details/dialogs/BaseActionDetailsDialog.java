69f4c3108c43d672678580650124701ce4c3582d
avkabanov <usrobotics1>
2019-03-02 23:07:30 +0300

    added date picker

--- app/src/main/java/com/kabanov/scheduler/action_details/dialogs/BaseActionDetailsDialog.java
+++ app/src/main/java/com/kabanov/scheduler/action_details/dialogs/BaseActionDetailsDialog.java
@@ -2,6 +2,7 @@ package com.kabanov.scheduler.action_details.dialogs;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.google.common.base.Strings;
@@ -11,11 +12,13 @@ import com.kabanov.scheduler.actions_table.ActionData;
 import com.kabanov.scheduler.add_action.NewAction;
 import com.kabanov.scheduler.add_action.ValidationException;
 
+import android.app.DatePickerDialog;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -28,6 +31,9 @@ public class BaseActionDetailsDialog {
     private final View popupView;
     private SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yy");
     protected final AlertDialog.Builder builder;
+
+    final Calendar myCalendar = Calendar.getInstance();
+
 
     public BaseActionDetailsDialog(MainActivity activity) {
         this.activity = activity;
@@ -42,7 +48,28 @@ public class BaseActionDetailsDialog {
         // Pass null as the parent view because its going in the dialog layout
         popupView = inflater.inflate(R.layout.add_action_popup, null);
 
+        EditText edittext= popupView.findViewById(R.id.executed_last_time_txt);
+        DatePickerDialog.OnDateSetListener date = (view, year, monthOfYear, dayOfMonth) -> {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        };
+        
+
+        edittext.setOnClickListener(v -> {
+            new DatePickerDialog(activity, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        });
+
         builder.setView(popupView);
+    }
+
+    private void updateLabel() {
+        ((TextView) popupView.findViewById(R.id.executed_last_time_txt))
+                .setText(formatLastExecutedDate(myCalendar.getTime()));
     }
     
     public void show() {
@@ -62,8 +89,6 @@ public class BaseActionDetailsDialog {
     String getDialogTitle() {
         return "Edit Action";
     }
-
-    
 
     private String formatLastExecutedDate(Date date) {
         return dateFormatter.format(date);
@@ -113,7 +138,7 @@ public class BaseActionDetailsDialog {
     }
 
     @NonNull
-    String getTextValue(int field) {
+    private String getTextValue(int field) {
         TextView text = popupView.findViewById(field);
         return text.getText().toString().trim();
     }
