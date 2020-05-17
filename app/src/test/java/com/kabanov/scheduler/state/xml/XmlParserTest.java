7f3b822cfdd94612ec979564c9624c62303edebc
avkabanov <TechnicalSkills10>
2020-05-17 12:23:01 +0300

    tests for xml formatter added

--- app/src/test/java/com/kabanov/scheduler/state/xml/XmlParserTest.java
+++ app/src/test/java/com/kabanov/scheduler/state/xml/XmlParserTest.java
@@ -1,33 +1,52 @@
 package com.kabanov.scheduler.state.xml;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import org.junit.Test;
 
+import com.kabanov.scheduler.actions_table.ActionData;
 import com.kabanov.scheduler.state.data.ApplicationState;
 
+import junit.framework.Assert;
+
 public class XmlParserTest {
-    
+
     private XmlParser xmlParser = new XmlParser();
-    
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
     @Test
     public void shouldFormatStateWhenValidStateIsGiven() throws Exception {
 
         ApplicationState result = xmlParser.format(state);
-        System.out.println(result);
-        
+
+        List<ActionData> actualActions = result.getActions();
+        Assert.assertEquals(2, actualActions.size());
+
+        ActionData firstAction = getActionWithId("action1", actualActions);
+        assertAction(firstAction, "action1", 45, "2020-02-09");
+
+        ActionData secondAction = getActionWithId("action2", actualActions);
+        assertAction(secondAction, "action2", 360, "2019-04-15");
+
     }
-    
+
+    private void assertAction(ActionData action, String id, int periodicity, String lastExecutionDate) throws ParseException {
+        Assert.assertEquals(id, action.getId());
+        Assert.assertEquals(periodicity, action.getPeriodicityDays());
+        Assert.assertEquals(simpleDateFormat.parse(lastExecutionDate), action.getLastExecutionDate());
+
+    }
+
+    private ActionData getActionWithId(String actionId, List<ActionData> actualActions) {
+        return actualActions.stream().filter(it -> it.getId().equals(actionId)).findAny().orElse(null);
+    }
+
     private String state = "<ApplicationState>\n" +
             "    <ActionDataStateList class=\"java.util.ArrayList\">\n" +
-            "        <actionDataState><id>action1</id><name>Подстричься</name><periodicityDays>45</periodicityDays><lastExecutionDate>2020-02-09</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action2</id><name>Гигиеническая чистка зубов</name><periodicityDays>360</periodicityDays><lastExecutionDate>2019-04-15</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action3</id><name>Помыть машину</name><periodicityDays>45</periodicityDays><lastExecutionDate>2020-03-01</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action4</id><name>Побриться</name><periodicityDays>7</periodicityDays><lastExecutionDate>2020-04-14</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action5</id><name>Кварплата Коммунарка</name><periodicityDays>30</periodicityDays><lastExecutionDate>2020-04-16</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action6</id><name>Почистить отпариватель</name><periodicityDays>75</periodicityDays><lastExecutionDate>2020-03-12</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action7</id><name>Почистить пылесос</name><periodicityDays>60</periodicityDays><lastExecutionDate>2020-04-11</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action8</id><name>Поменять фильтр в пылесосе</name><periodicityDays>90</periodicityDays><lastExecutionDate>2020-04-11</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action9</id><name>Поменять зубную щетку</name><periodicityDays>90</periodicityDays><lastExecutionDate>2020-04-28</lastExecutionDate></actionDataState>\n" +
-            "        <actionDataState><id>action10</id><name>Поменять накладки</name><periodicityDays>180</periodicityDays><lastExecutionDate>2020-01-31</lastExecutionDate></actionDataState>\n" +
+            "        <actionDataState><id>action1</id><name>firstActionName</name><periodicityDays>45</periodicityDays><lastExecutionDate>2020-02-09</lastExecutionDate></actionDataState>\n" +
+            "        <actionDataState><id>action2</id><name>secondActionName</name><periodicityDays>360</periodicityDays><lastExecutionDate>2019-04-15</lastExecutionDate></actionDataState>\n" +
             "    </ActionDataStateList>\n" +
             "</ApplicationState>";
 }
